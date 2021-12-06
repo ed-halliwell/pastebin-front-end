d9d69d57768d958c299c92f3640458860f9cfd0f
Ed Halliwell <edhalliwell@hotmail.com>
2021-12-06 16:33:13 +0000

    Set up initial skeleton, pull data in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import { greet } from "./utils/greet";
+import { useState } from "react";
+import axios from "axios";
+import { ISnippet } from "./utils/interfaces";
+import SnippetList from "./components/SnippetList";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  const [snippets, setSnippets] = useState<ISnippet[]>([]);
+  const loadDataFromEndpoint = async (endpoint: string) => {
+    try {
+      const res = await axios.get(
+        `https://pastebin-academy.herokuapp.com/${endpoint}`
+      );
+      // console.log(res.data.data);
+      setSnippets(res.data.data);
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(`${err.name}: ${err.message}`);
+      }
+    }
+  };
+  loadDataFromEndpoint("snippets");
+
+  return (
+    <>
+      <SnippetList snippets={snippets} />
+    </>
+  );
 }
 
 export default App;
+
+// App
+//   > Form - creating a snippet
+//   > List of Snippets (ordered by createdAt desc)
+//     > Snippet - can be expanded
