84e4cad8ec459245f9a6f8a988c9237480515446
Ed Halliwell <edhalliwell@hotmail.com>
2021-12-08 08:46:59 +0000

    Various styling changes

--- src/components/ViewSnippet.tsx
+++ src/components/ViewSnippet.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { ISnippet } from "../utils/interfaces";
+import "../styles/ViewSnippet.css";
 
 interface Props {
   snippet: ISnippet;
@@ -36,75 +37,79 @@ export default function ViewSnippet(props: Props): JSX.Element {
   return (
     <>
       {edit ? (
-        <div className="col-6 mx-4 w-100">
+        <div>
           <h4 className="mb-4">Edit Snippet</h4>
-          <form onSubmit={handleUpdate}>
+          <div className="col-6 mx-4 w-100 view-box">
+            <form onSubmit={handleUpdate}>
+              <div className="mb-3">
+                <label htmlFor="snippet-title" className="form-label">
+                  Snippet Title
+                </label>
+                <input
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  type="text"
+                  placeholder="Add a title"
+                  className="form-control"
+                  id="snippet-title"
+                  aria-describedby="snippet-title"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="snippet-text" className="form-label">
+                  Snippet Text
+                </label>
+                <div className="input-group create-snippet-box">
+                  <textarea
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Paste something..."
+                    id="snippet-text"
+                    className="form-control snippet-text"
+                    aria-label="snippet"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button
+                  className="btn btn-secondary me-md-2"
+                  onClick={() => setEdit(false)}
+                >
+                  Cancel
+                </button>
+                <button className="btn btn-primary me-md-2" type="submit">
+                  Update
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h4 className="mb-4 mx-4">View Snippet</h4>
+          <div className="col-6 mx-4 w-100 view-box">
             <div className="mb-3">
               <label htmlFor="snippet-title" className="form-label">
                 Snippet Title
               </label>
-              <input
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                type="text"
-                placeholder="Add a title"
-                className="form-control"
-                id="snippet-title"
-                aria-describedby="snippet-title"
-              />
+              <p>{props.snippet.title}</p>
             </div>
             <div className="mb-3">
               <label htmlFor="snippet-text" className="form-label">
                 Snippet Text
               </label>
-              <div className="input-group">
-                <textarea
-                  value={text}
-                  onChange={(e) => setText(e.target.value)}
-                  placeholder="Paste something..."
-                  id="snippet-text"
-                  className="form-control"
-                  aria-label="snippet"
-                ></textarea>
+              <div className="snippet-box">
+                <p className="snippet-text">{props.snippet.text}</p>
               </div>
             </div>
             <div className="d-grid gap-2 d-md-flex justify-content-md-end">
               <button
                 className="btn btn-secondary me-md-2"
-                onClick={() => setEdit(false)}
+                onClick={() => setEdit(true)}
               >
-                Cancel
-              </button>
-              <button className="btn btn-primary me-md-2" type="submit">
-                Update
+                Edit
               </button>
             </div>
-          </form>
-        </div>
-      ) : (
-        <div className="col-6 mx-4 w-100">
-          <h4 className="mb-4">View Snippet</h4>
-          <div className="mb-3">
-            <label htmlFor="snippet-title" className="form-label">
-              Snippet Title
-            </label>
-            <p>{props.snippet.title}</p>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="snippet-text" className="form-label">
-              Snippet Text
-            </label>
-            <div>
-              <p>{props.snippet.text}</p>
-            </div>
-          </div>
-          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button
-              className="btn btn-secondary me-md-2"
-              onClick={() => setEdit(true)}
-            >
-              Edit
-            </button>
           </div>
         </div>
       )}
